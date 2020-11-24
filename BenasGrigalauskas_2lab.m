04a4df33306fb4829fd89abf210919864336ccc7
Benas Grigalauskas <73246949+BenasGri@users.noreply.github.com>
2020-11-24 20:16:37 +0200

    tinklo atsako skaiciavimas

--- BenasGrigalauskas_2lab.m
+++ BenasGrigalauskas_2lab.m
@@ -23,6 +23,14 @@ w24 = rand(1);
 %antro sluoksnio bias
 b21 = rand(1);
 
+for i = 1:20
+    %Tinklo atsako skaiciavimas
+    v11 = x(i)*w11+b11;
+    v12 = x(i)*w12+b12;
+    v13 = x(i)*w13+b13;
+    v14 = x(i)*w14+b14;
+end
+
 function y = f1(x)
     y = (1 + 0.6 * sin((2*pi*x)/0.7)) + ((0.3 * sin(2*pi*x))/2);
 end
