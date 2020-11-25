c2d1c5faec17606a41728e59c5781c2ab21b5946
Benas Grigalauskas <73246949+BenasGri@users.noreply.github.com>
2020-11-25 19:47:43 +0200

    algoritmas įstatytas į ciklą ir bei atvaizduojami rezultatai

--- BenasGrigalauskas_2lab.m
+++ BenasGrigalauskas_2lab.m
@@ -24,41 +24,54 @@ w24 = rand(1);
 b21 = rand(1);
 
 ypgr = f1(x);   %pagrindines funkcijos rezultatai
-eta = 0.2;      %mokymosi greitis
+eta = 0.8;      %mokymosi greitis
 
-for i = 1:20
-    %Tinklo atsako skaiciavimas
-    v11 = x(i)*w11+b11;
-    v12 = x(i)*w12+b12;
-    v13 = x(i)*w13+b13;
-    v14 = x(i)*w14+b14;
-    %Aktyvaviams
-    y1 = 1/(1+exp((-1)*v11));
-    y2 = 1/(1+exp((-1)*v12));
-    y3 = 1/(1+exp((-1)*v13));
-    y4 = 1/(1+exp((-1)*v14));
-    %sigmoidine aktyvavimo funkcija
-    y(i) = y1*w21 + y2*w22 + y3*w23 + y4*w24 + b21;
-    %klaidos radimas
-    e(i) = ypgr(i) - y(i);
-    %antro sluoksnio parametru atnaujimas
-    w21 = w21 + eta*e(i)*x(i);
-    w22 = w22 + eta*e(i)*x(i);
-    w23 = w23 + eta*e(i)*x(i);
-    w24 = w24 + eta*e(i)*x(i);
-    
-    b21 = b21 + eta*e(i);
-    %pirmo sluoksnio parametru atnaujinimas
-    w11 = w11 + eta*e(i)*y1*(1-y1)*w21*x(i);
-    w12 = w12 + eta*e(i)*y2*(1-y2)*w22*x(i);
-    w13 = w13 + eta*e(i)*y3*(1-y3)*w23*x(i);
-    w14 = w14 + eta*e(i)*y4*(1-y4)*w24*x(i);
-    
-    b11 = b11 + eta*e(i)*y1*(1-y1)*w21;
-    b12 = b12 + eta*e(i)*y2*(1-y2)*w22;
-    b13 = b13 + eta*e(i)*y3*(1-y3)*w23;
-    b14 = b14 + eta*e(i)*y4*(1-y4)*w24;
+for n = 1:10000
+    for i = 1:20
+        %Tinklo atsako skaiciavimas
+        v11 = x(i)*w11+b11;
+        v12 = x(i)*w12+b12;
+        v13 = x(i)*w13+b13;
+        v14 = x(i)*w14+b14;
+        %Aktyvaviams
+        y1 = 1/(1+exp((-1)*v11));
+        y2 = 1/(1+exp((-1)*v12));
+        y3 = 1/(1+exp((-1)*v13));
+        y4 = 1/(1+exp((-1)*v14));
+        %sigmoidine aktyvavimo funkcija
+        y(i) = y1*w21 + y2*w22 + y3*w23 + y4*w24 + b21;
+        %klaidos radimas
+        e(i) = ypgr(i) - y(i);
+        %antro sluoksnio parametru atnaujimas
+        w21 = w21 + eta*e(i)*x(i);
+        w22 = w22 + eta*e(i)*x(i);
+        w23 = w23 + eta*e(i)*x(i);
+        w24 = w24 + eta*e(i)*x(i);
+
+        b21 = b21 + eta*e(i);
+        %pirmo sluoksnio parametru atnaujinimas
+        w11 = w11 + eta*e(i)*y1*(1-y1)*w21*x(i);
+        w12 = w12 + eta*e(i)*y2*(1-y2)*w22*x(i);
+        w13 = w13 + eta*e(i)*y3*(1-y3)*w23*x(i);
+        w14 = w14 + eta*e(i)*y4*(1-y4)*w24*x(i);
+
+        b11 = b11 + eta*e(i)*y1*(1-y1)*w21;
+        b12 = b12 + eta*e(i)*y2*(1-y2)*w22;
+        b13 = b13 + eta*e(i)*y3*(1-y3)*w23;
+        b14 = b14 + eta*e(i)*y4*(1-y4)*w24;
+    end
 end
+
+%Rezultatu isvedimas
+figure(1);
+hold on
+plot(x,ypgr)
+plot(x,y)
+hold off
+legend('Pagrindine funkcija','Aproksimuota funkcija');
+xlabel('x');
+ylabel('y');
+grid on;
 
 function y = f1(x)
     y = (1 + 0.6 * sin((2*pi*x)/0.7)) + ((0.3 * sin(2*pi*x))/2);
